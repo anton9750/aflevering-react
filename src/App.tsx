fbbbdee9c5a6a232e52868deae462b1f137bf256
unknown <you>
2026-08-10 11:44:17 +0200

    i added the structure

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import "./App.css"
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+import { Main } from "./components/main";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const App = () => {
+return (
+
+ 
+  <>
+
+  <Footer/>
+
+  <Main/>
+
+  <Header/>
+  <a>
+    <button>¨
+      siden
+    </button>
+    
+  </a>
+
+  <a>
+    <button>
+      <h3>
+        main
+      </h3>
+    </button>
+  </a>
+
+  <footer>
+  <a>
+    <button>
+      <div className="footer">
+        <h3>
+          footer
+        </h3>
+      </div>
+    </button>
+
+  </a>
+  
+  </footer>
+
+
+
+
+  </>
+)
+
+
+
+
+}
+
+export default App;
+
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <header>
+            hello
+        </header>
+    )
+}
+
